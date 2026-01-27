4e2daf0a3e6614ec1260e3d959aab766a9cb720d
fernandamscabral <nanda.sousa.m@gmail.com>
2026-01-27 18:03:58 -0300

    test: add GET /posts/:id positive and negative scenarios

--- tests/posts.spec.js
+++ tests/posts.spec.js
@@ -17,3 +17,29 @@ test('GET /posts should return a list of posts', async ({ request }) => {
   expect(body[0]).toHaveProperty('title');
   expect(body[0]).toHaveProperty('body');
 });
+
+test('GET /posts/1 should return a single post', async ({ request }) => {
+  const response = await request.get(
+    'https://jsonplaceholder.typicode.com/posts/1'
+  );
+
+  expect(response.status()).toBe(200);
+
+  const body = await response.json();
+
+  expect(body.id).toBe(1);
+  expect(body).toHaveProperty('userId');
+  expect(body).toHaveProperty('title');
+  expect(body).toHaveProperty('body');
+});
+
+test('GET /posts/9999 should return empty response', async ({ request }) => {
+  const response = await request.get(
+    'https://jsonplaceholder.typicode.com/posts/9999'
+  );
+
+  expect(response.status()).toBe(404);
+
+  const body = await response.json();
+  expect(body).toEqual({});
+});
