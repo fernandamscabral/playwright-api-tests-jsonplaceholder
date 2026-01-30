d104533147cc011b881bb31dbe6b25b120acbe14
fernandamscabral <nanda.sousa.m@gmail.com>
2026-01-30 12:14:59 -0300

    chore: add @sanity tag to sanity test

--- tests/sanity.spec.js
+++ tests/sanity.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('GET /posts - api is reachable (sanity)', async ({ request }) => {
+test('@sanity GET /posts - api is reachable (sanity)', async ({ request }) => {
   const response = await request.get('/posts');
-  
+
   expect(response.status()).toBe(200);
 });
